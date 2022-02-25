a4c277c396501ef6c3ef7742b680634b4bd88c8f
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2022-02-24 22:27:56 -0300

    Progress checklist: 2. Finished adding the viewport, as well enabled cornerstone to display images.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import cornerstone from 'cornerstone-core';
+import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import dicomParser from 'dicom-parser';
+
 import './App.css';
 
-// TODO: init cornerstone
-// const init = () => {};
+const displayImage = async (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+
+  const file = event.dataTransfer.files[0];
+  const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+
+  try {
+    const viewer = event.target;
+    cornerstone.enable(viewer);
+
+    const image = await cornerstone.loadImage(imageId);
+    const cornerstoneViewport = cornerstone.getDefaultViewportForImage(viewer, image);
+    cornerstone.displayImage(viewer, image, cornerstoneViewport);
+  } catch(err) {
+    console.error(err);
+  }
+};
+const init = () => {
+  cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+  cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+};
+const stopEvent = (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+};
 
 function App() {
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div className="App">
       <div className='toolbar'></div>
 
-      <main className='viewport'></main>
+      <main
+        id='viewport'
+        onDragOver={stopEvent}
+        onDrop={displayImage}
+      />
 
       <div className='buttons'>
         <button>Load image</button>
